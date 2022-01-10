4c0d78fca062b00cb37da1655b32a6f27090fc86
Akshay Rangasai <arangasai@Akshays-Powerful-GIF-Maker.local>
2022-01-10 18:35:12 -0500

    Added Spotify API JS Node Library to packagaes.json

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+var postsRouter = require("./routes/posts");
 const app = express();
 // Use __CAPS for constants and paths
 const __PORT = 3000;
 
-app.get('/', (req, res) => res.send("Hello to you too"))
+app.get('/', (req, res) => res.send("Hello to you too"));
+app.use('/posts', postsRouter);
 
 app.listen(__PORT, () => console.log("Listening on port", __PORT));
